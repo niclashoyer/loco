b2fe6b7fa698def05fb47adbe4d26caab07d68d5
Niclas Hoyer <niclas@verbugt.de>
2021-01-06 00:31:47 +0100

    susi: tests: add clock simulation based tests

--- susi/tests/send_and_receive.rs
+++ susi/tests/send_and_receive.rs
@@ -1,22 +1,24 @@
-mod simulation;
+mod time;
 mod wire;
-use simulation::*;
+use embedded_time::duration::*;
+use time::*;
 
 use susi::message::{Direction, Msg};
-
-use embedded_time::duration::*;
 
 type SusiSender = susi::sender::Sender<OpenDrainPin, PushPullPin, SimTimer>;
 type SusiReceiver = susi::receiver::Receiver<OpenDrainPin, InputOnlyPin, SimTimer>;
 
-fn send_and_receive<FS: 'static, FR: 'static>(send: FS, receive: FR) -> Vec<Msg>
-where
-	FS: FnOnce(SusiSender) -> () + Send,
-	FR: FnOnce(SusiReceiver) -> Vec<Msg> + Send,
-{
-	use std::thread::sleep;
-	use std::time::Duration;
+enum Error {}
 
+fn send_and_receive<FS: 'static, FR: 'static>(
+	mut send: FS,
+	mut receive: FR,
+	timeout: u32,
+) -> Vec<Msg>
+where
+	FS: FnMut(&mut SusiSender) -> nb::Result<(), Error>,
+	FR: FnMut(&mut SusiReceiver) -> nb::Result<Vec<Msg>, Error>,
+{
 	let wire_clk = Wire::new();
 	let wire_data = Wire::new_with_pull(WireState::High);
 
@@ -26,85 +28,77 @@ where
 	let receiver_pin_clk = wire_clk.as_input_pin();
 	let receiver_pin_data = wire_data.as_open_drain_pin();
 
-	let sender = thread::spawn(move || {
-		set_realtime_priority(80);
-		let timer = hal::SysTimer::new();
-		let timer = UsToStdCountDown::from(timer);
-		let sender = susi::sender::Sender::new(sender_pin_data, sender_pin_clk, timer);
-		sleep(Duration::from_millis(200));
-		send(sender)
-	});
-	let receiver = thread::spawn(move || {
-		set_realtime_priority(90);
-		let timer = hal::SysTimer::new();
-		let timer = MsToStdCountDown::from(timer);
-		let receiver = susi::receiver::Receiver::new(receiver_pin_data, receiver_pin_clk, timer);
-		receive(receiver)
-	});
-	let rec = receiver.join().unwrap();
-	sender.join().unwrap();
-	rec
+	let mut clock = SimClock::new();
+	let sender_timer = clock.get_timer();
+	let receiver_timer = clock.get_timer();
+
+	let mut sender = susi::sender::Sender::new(sender_pin_data, sender_pin_clk, sender_timer);
+	let mut receiver =
+		susi::receiver::Receiver::new(receiver_pin_data, receiver_pin_clk, receiver_timer);
+
+	let mut recv = vec![];
+	let mut sender_done = false;
+	let mut receiver_done = false;
+
+	loop {
+		if clock.elapsed() > timeout.milliseconds() {
+			panic!("simulation timed out");
+		}
+		if !sender_done {
+			if let Ok(_) = send(&mut sender) {
+				sender_done = true;
+				println!("sender done");
+			}
+		}
+		if !receiver_done {
+			if let Ok(msgs) = receive(&mut receiver) {
+				recv = msgs;
+				receiver_done = true;
+				println!("receiver done");
+			}
+		}
+		if sender_done && receiver_done {
+			break;
+		}
+		clock.tick(100_u64.nanoseconds());
+	}
+	recv
 }
 
 fn send_and_receive_messages(msgs: Vec<Msg>) {
-	use std::thread::{sleep, yield_now};
-	use std::time::Duration;
-
 	let mut send_msgs = msgs.clone();
+	send_msgs.reverse();
 	let num = msgs.len();
+	let mut msg = send_msgs.pop().expect("at least one message must be sent");
+	let mut recv = vec![];
 
-	let sender = move |mut sender: SusiSender| {
-		send_msgs.reverse();
-		sleep(Duration::from_millis(200));
-		while let Some(msg) = send_msgs.pop() {
-			loop {
-				let res = sender.write(&msg);
-				if let Ok(_) = res {
-					break;
-				} else if res != Err(nb::Error::WouldBlock) {
-					panic!(res);
-				}
-				let slept = std::time::Instant::now();
-				sleep(Duration::from_micros(50));
-				let slept = slept.elapsed().as_micros();
-				if slept > 200 {
-					eprintln!(
-						"WARNING: sender slept {} µs, more than 200 µs will cause problems",
-						slept
-					);
-				}
+	let sender = move |sender: &mut SusiSender| {
+		let res = sender.write(&msg);
+		if let Ok(_) = res {
+			if send_msgs.is_empty() {
+				return Ok(());
+			} else {
+				msg = send_msgs.pop().unwrap();
 			}
+		} else if res != Err(nb::Error::WouldBlock) {
+			panic!(res);
 		}
+		Err(nb::Error::WouldBlock)
 	};
-	let receiver = move |mut receiver: SusiReceiver| {
-		let start = std::time::Instant::now();
-		let mut recv = vec![];
-		loop {
-			if start.elapsed().as_millis() > 2000 {
-				panic!("receiver timed out - buf: {:?}", recv);
+	let receiver = move |receiver: &mut SusiReceiver| {
+		let res = receiver.read();
+		if let Ok(msg) = res {
+			recv.push(msg);
+			if recv.len() == num {
+				return Ok(recv.clone());
 			}
-			let res = receiver.read();
-			if let Ok(msg) = res {
-				recv.push(msg);
-				if recv.len() == num {
-					return recv;
-				}
-			} else if res != Err(nb::Error::WouldBlock) {
-				panic!(res);
-			}
-			let slept = std::time::Instant::now();
-			sleep(Duration::from_micros(50));
-			let slept = slept.elapsed().as_micros();
-			if slept > 200 {
-				eprintln!(
-					"WARNING: receiver slept {} µs, more than 200 µs will cause problems",
-					slept
-				);
-			}
+		} else if res != Err(nb::Error::WouldBlock) {
+			panic!(res);
 		}
+		Err(nb::Error::WouldBlock)
 	};
 
-	let recv = send_and_receive(sender, receiver);
+	let recv = send_and_receive(sender, receiver, 500);
 	assert_eq!(recv, msgs);
 }
 
