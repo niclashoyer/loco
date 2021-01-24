0f586c693bf4015807f11816b317fed001117391
Niclas Hoyer <info@niclashoyer.de>
2021-01-25 00:18:24 +0100

    xpressnet: Message enum complete

--- xpressnet/src/lib.rs
+++ xpressnet/src/lib.rs
@@ -1,10 +1,10 @@
 use bitflags::bitflags;
-use heapless::consts::U17;
-use heapless::Vec;
-
-pub trait Bits<T>: Copy {
-	fn bits(&self) -> T;
-}
+use loco_core::{
+	drive::{Direction, Speed},
+	functions::FunctionGroupNumber,
+	mov, xor, Bits,
+};
+use loco_dcc::FunctionGroupByte;
 
 bitflags! {
 	pub struct CentralState: u8 {
@@ -24,25 +24,30 @@ impl Bits<u8> for CentralState {
 }
 
 #[derive(Debug)]
-pub struct MessageBytes {
-	bytes: Vec<u8, U17>,
+pub struct Accessory {
+	address: u8,
+	data: u8,
 }
 
-impl MessageBytes {
-	fn from_slice(slice: &[u8]) -> Self {
-		Self {
-			bytes: Vec::<_, _>::from_slice(slice).unwrap(),
-		}
-	}
+#[derive(Debug)]
+pub enum SearchResult {
+	Loco(u16),
+	DoubleHeading(u16),
+	ConsistBase(u16),
+	Consist(u16),
+	None,
+}
 
-	fn xor(&self) -> u8 {
-		let mut iter = self.bytes[0..].iter();
-		let mut xor = *iter.next().unwrap();
-		for byte in iter {
-			xor = xor ^ byte;
-		}
-		xor
-	}
+#[derive(Debug)]
+pub enum CentralError {
+	ConsistError,
+	ConsistOccupied,
+	AlreadyInConsist,
+	ConsistSpeedNotZero,
+	LocoNotInConsist,
+	NoConsistBase,
+	DeleteNotPossible,
+	StackOverflow,
 }
 
 #[derive(Debug)]
@@ -50,12 +55,63 @@ pub enum CentralMessage<S: Bits<u8>> {
 	TrackPowerOn,
 	TrackPowerOff,
 	EmergencyStop,
-	//FeedbackBroadcast([(u8, u8); 7])
+	ProgrammingModeOn,
+	FeedbackBroadcast([(u8, u8); 7]),
+	ProgrammingShortCircuit,
+	ProgrammingNoData,
+	ProgrammingBusy,
+	ProgrammingReady,
+	ProgrammingDataPaged(u8, u8),
+	ProgrammingDataDirect(u16, u8),
 	Version(u8, u8),
 	State(S),
 	TransferError,
 	StationBusy,
 	UnknownCommand,
+	AccessoryResponse(Accessory), // FIXME
+	LocoInformation {
+		is_free: bool,
+		direction: Direction,
+		speed: Speed,
+		f0: FunctionGroupByte,
+		f1: FunctionGroupByte,
+	},
+	FunctionState {
+		f3: FunctionGroupByte,
+		f4: FunctionGroupByte,
+	},
+	LocoConsistInformation {
+		is_free: bool,
+		direction: Direction,
+		speed: Speed,
+		f0: FunctionGroupByte,
+		f1: FunctionGroupByte,
+		consist_address: u8,
+	},
+	LocoConsistBaseInformation {
+		is_free: bool,
+		direction: Direction,
+		speed: Speed,
+	},
+	LocoDoubleHeadingInformation {
+		is_free: bool,
+		direction: Direction,
+		speed: Speed,
+		f0: FunctionGroupByte,
+		f1: FunctionGroupByte,
+		other_address: u16,
+	},
+	LocoOccupied(u16),
+	FunctionToggled0 {
+		f0: FunctionGroupByte,
+		f1: FunctionGroupByte,
+	},
+	FunctionToggled1 {
+		f2: FunctionGroupByte,
+		f3: FunctionGroupByte,
+	},
+	SearchResult(SearchResult),
+	Error(CentralError),
 }
 
 #[derive(Debug)]
@@ -63,47 +119,106 @@ pub enum Error {
 	ParseError,
 }
 
-macro_rules! xor {
-	( [$x:expr, $( $y:expr ),*] ) => {
-		[$x,$( $y ),*,$x $( ^$y )*]
-	}
-}
-
-macro_rules! mb {
-	( $b:ident[$p:expr] <= $($x:tt)* ) => {{
-		$b[$p].copy_from_slice($($x)*);
-		$b[$p].len()
-	}};
-}
-
 impl<S: Bits<u8>> CentralMessage<S> {
 	pub fn to_buf(&self, buf: &mut [u8]) -> usize {
 		use CentralMessage::*;
 		match self {
-			&TrackPowerOn => mb!(buf[0..3] <= &xor!([0x61, 0x01])),
-			&TrackPowerOff => mb!(buf[0..3] <= &xor!([0x61, 0x00])),
-			&EmergencyStop => mb!(buf[0..3] <= &xor!([0x81, 0x00])),
-			&Version(u, l) => mb!(buf[0..5] <= &xor!([0x63, 0x21, u, l])),
-			&State(state) => mb!(buf[0..4] <= &xor!([0x62, 0x22, state.bits()])),
-			&TransferError => mb!(buf[0..3] <= &xor!([0x61, 0x80])),
-			&StationBusy => mb!(buf[0..3] <= &xor!([0x61, 0x81])),
-			&UnknownCommand => mb!(buf[0..3] <= &xor!([0x61, 0x82])),
+			TrackPowerOn => mov!(buf[0..3] <= &xor!([0x61, 0x01])),
+			TrackPowerOff => mov!(buf[0..3] <= &xor!([0x61, 0x00])),
+			EmergencyStop => mov!(buf[0..3] <= &xor!([0x81, 0x00])),
+			Version(u, l) => mov!(buf[0..5] <= &xor!([0x63, 0x21, *u, *l])),
+			State(state) => mov!(buf[0..4] <= &xor!([0x62, 0x22, state.bits()])),
+			TransferError => mov!(buf[0..3] <= &xor!([0x61, 0x80])),
+			StationBusy => mov!(buf[0..3] <= &xor!([0x61, 0x81])),
+			UnknownCommand => mov!(buf[0..3] <= &xor!([0x61, 0x82])),
+			_ => unimplemented!(),
 		}
 	}
 }
 
 #[derive(Debug)]
+pub enum RefreshMode {
+	F0ToF4 = 0x0,
+	F0ToF8 = 0x1,
+	F0ToF12 = 0x3,
+	F0ToF20 = 0x7,
+	F0ToF28 = 0xF,
+}
+
+#[derive(Debug)]
 pub enum DeviceMessage {
+	TrackPowerOn,
+	TrackPowerOff,
+	EmergencyStop,
+	LocoEmergencyStop(u16),
+	ProgrammingReadRegister(u8),
+	ProgrammingReadDirect(u16),
+	ProgrammingReadPaged(u8),
+	ProgrammingGetResult,
+	ProgrammingWriteRegister(u8, u8),
+	ProgrammingWriteDirect(u16, u8),
+	ProgrammingWritePaged(u8, u8),
 	GetVersion,
 	GetState,
+	GetAccessory(Accessory),     // FIXME
+	ControlAccessory(Accessory), // FIXME
+	GetLocoInformation(u16),
+	GetFunctionToggled0(u16),
+	GetFunctionToggled1(u16),
+	GetFunctionState(u16),
+	LocoDrive(u16, Direction, Speed),
+	SetFunctionGroup(FunctionGroupNumber, FunctionGroupByte),
+	SetFunctionToggled(FunctionGroupNumber, FunctionGroupByte),
+	SetRefreshMode(RefreshMode),
+	AddDoubleHeading(u16, u16),
+	RemoveDoubleHeading(u16),
+	ProgrammingOnMainWrite {
+		loco_address: u16,
+		cv_address: u16,
+		value: u8,
+	},
+	ProgrammingOnMainRead {
+		loco_address: u16,
+		cv_address: u16,
+		value: u8,
+	},
+	ProgrammingOnMainWriteBit {
+		loco_address: u16,
+		cv_address: u16,
+		position: u8,
+		value: bool,
+	},
+	AddConsist {
+		inverted: bool,
+		loco_address: u16,
+		base_address: u8,
+	},
+	RemoveConsist {
+		loco_address: u16,
+		base_address: u8,
+	},
+	SearchConsistMember {
+		forward: bool,
+		loco_address: u16,
+		base_address: u8,
+	},
+	SearchConsistBase {
+		forward: bool,
+		base_address: u8,
+	},
+	SearchLocoInStack {
+		forward: bool,
+		loco_address: u16,
+	},
+	RemoveFromStack(u16),
 }
 
 impl DeviceMessage {
 	pub fn from_bytes(bytes: &[u8]) -> Result<DeviceMessage, Error> {
 		use DeviceMessage::*;
 		match bytes {
-			&[0x21, 0x21, 0x00, ..] => Ok(GetVersion),
-			&[0x21, 0x24, 0x05, ..] => Ok(GetState),
+			[0x21, 0x21, 0x00, ..] => Ok(GetVersion),
+			[0x21, 0x24, 0x05, ..] => Ok(GetState),
 			_ => {
 				println!("X: {:#04X?}", bytes);
 				Err(Error::ParseError)
