25529177e9c897abdefdde16f21087bc2437bf07
Niclas Hoyer <info@niclashoyer.de>
2021-01-25 22:23:01 +0100

    dcc: Add speed parsing

--- dcc/src/lib.rs
+++ dcc/src/lib.rs
@@ -1,12 +1,13 @@
 use loco_core::drive::Direction;
 
-pub trait Byte<T> {
-	fn from_byte(byte: &u8) -> T;
+pub trait DirectionByte {
+	fn from_byte(byte: u8) -> Direction;
 	fn to_byte(&self) -> u8;
 }
 
-impl Byte<Direction> for Direction {
-	fn from_byte(val: &u8) -> Self {
+impl DirectionByte for Direction {
+	#[inline]
+	fn from_byte(val: u8) -> Self {
 		if val & (1 << 5) != 0 {
 			Self::Forward
 		} else {
@@ -14,10 +15,64 @@ impl Byte<Direction> for Direction {
 		}
 	}
 
+	#[inline]
 	fn to_byte(&self) -> u8 {
 		match self {
 			Direction::Forward => 0x20,
 			Direction::Backward => 0x00,
+		}
+	}
+}
+
+use loco_core::drive::Speed;
+
+pub trait SpeedByte {
+	fn from_byte_14_steps(byte: u8) -> Speed;
+	fn from_byte_28_steps(byte: u8) -> Speed;
+	fn from_byte_128_steps(byte: u8) -> Speed;
+	fn to_byte(&self) -> u8;
+}
+
+impl SpeedByte for Speed {
+	#[inline]
+	fn from_byte_14_steps(byte: u8) -> Speed {
+		use Speed::*;
+		match byte & 0x0F {
+			0x0 => Stop,
+			0x01 => EmergencyStop,
+			s => Steps14(s * 8),
+		}
+	}
+
+	#[inline]
+	fn from_byte_28_steps(byte: u8) -> Speed {
+		use Speed::*;
+		match byte & 0x0F {
+			0x00 => Stop,
+			0x01 => EmergencyStop,
+			s => Steps28(((s << 1) | ((byte >> 4) & 0x01)) * 4),
+		}
+	}
+
+	#[inline]
+	fn from_byte_128_steps(byte: u8) -> Speed {
+		use Speed::*;
+		match byte & 0x0F {
+			0x0 => Stop,
+			0x01 => EmergencyStop,
+			_ => Steps128((byte & 0x7F) * 2),
+		}
+	}
+
+	#[inline]
+	fn to_byte(&self) -> u8 {
+		use Speed::*;
+		match self {
+			Stop => 0x00,
+			EmergencyStop => 0x01,
+			Steps14(s) => (s / 4) & 0x0F,
+			Steps28(s) => (((s / 8) & 0x0F) << 1) | (((s / 4) & 0x01) << 4),
+			Steps128(s) => (s / 2) & 0x7F,
 		}
 	}
 }
@@ -30,6 +85,7 @@ pub struct FunctionGroupByte {
 }
 
 impl FunctionGroupByte {
+	#[inline]
 	fn function_position(f: Function) -> u8 {
 		use num_traits::ToPrimitive;
 		let n = f.to_u8().unwrap();
@@ -40,11 +96,13 @@ impl FunctionGroupByte {
 		}
 	}
 
+	#[inline]
 	pub fn get(&self, f: Function) -> bool {
 		let p = Self::function_position(f);
 		(self.data >> p) & 0x01 == 0x01
 	}
 
+	#[inline]
 	pub fn set(&mut self, f: Function, value: bool) {
 		let p = Self::function_position(f);
 		if value {
@@ -54,18 +112,21 @@ impl FunctionGroupByte {
 		}
 	}
 
+	#[inline]
 	pub fn clear(&mut self) {
 		self.data = 0x00;
 	}
 }
 
 impl From<u8> for FunctionGroupByte {
+	#[inline]
 	fn from(data: u8) -> Self {
 		Self { data }
 	}
 }
 
 impl From<FunctionGroupByte> for u8 {
+	#[inline]
 	fn from(data: FunctionGroupByte) -> u8 {
 		data.data
 	}
