31a6a703394977928f7703c637a56bb2da3701c0
Niclas Hoyer <info@niclashoyer.de>
2021-12-23 20:03:55 +0100

    core: Add address

---
+++ core/src/address.rs
@@ -0,0 +1,4 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Address {
+	pub num: u16,
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod address;
 pub mod analog;
 pub mod drive;
 pub mod functions;
@@ -5,4 +6,11 @@ pub mod macros;
 
 pub trait Bits<T>: Copy {
 	fn bits(&self) -> T;
+}
+
+#[inline]
+pub fn add_xor(buf: &mut [u8], len: usize) -> usize {
+	let x = buf[0..len - 1].iter().fold(0, |acc, x| acc ^ x);
+	buf[len - 1] = x;
+	len
 }
