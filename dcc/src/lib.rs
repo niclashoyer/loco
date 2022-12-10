e57a2d554a5fa2f24f640f3704bf0e5207378d34
Niclas Hoyer <info@niclashoyer.de>
2022-12-11 00:41:55 +0100

    [WIP] dcc: async writer

--- dcc/src/lib.rs
+++ dcc/src/lib.rs
@@ -1,13 +1,16 @@
+#![allow(incomplete_features)]
+#![feature(async_fn_in_trait)]
+
 pub mod address;
 pub mod direction;
 pub mod function;
 pub mod message;
-pub mod reader;
+//pub mod reader;
 pub mod speed;
 pub mod writer;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
     IOError,
-    TimerError,
+    DelayError,
 }
